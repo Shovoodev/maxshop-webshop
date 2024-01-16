b8d567240757e3f2ccb44714e6f72eca82161bc6
toukirislam <shovoodev@gmail.com>
2024-01-16 12:21:33 +0100

    adding and fatching data

--- frontend/src/screens/Homescreen.jsx
+++ frontend/src/screens/Homescreen.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+
+import { useEffect ,useState } from 'react';
 import { Row , Col } from 'react-bootstrap'
 import Products from '../components/Products.jsx';
-import products from '../products.js';
+import Axios from 'axios'
 
 const Homescreen = () => {
+  const [product , setProduct] = useState([])
+
+  useEffect(()=> {
+    const fetchProducts = async () => {
+      const {data} = await Axios.get('/api/products')
+      setProduct(data)
+    }
+    fetchProducts();
+  }, [])
+
   return (
     <>
         <h1>latest products</h1>
         <Row>
-            {products.map((product) => {
+            {product.map((product) => {
                return  <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Products  product={product}/>
                 </Col>
